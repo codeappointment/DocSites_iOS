1936a3df6814ff621889b5dacc19bee9611c8843
Shahriar Nasim Nafi <shahriarnasim.nafi@gmail.com>
2021-04-15 18:02:27 +0600

    Initial Commit

---
+++ Shared/DocSitesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocSitesApp.swift
+//  Shared
+//
+//  Created by Shahriar Nasim Nafi on 15/4/21.
+//
+
+import SwiftUI
+
+@main
+struct DocSitesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
